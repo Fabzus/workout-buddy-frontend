3556623663814547aed34c6bdcbd9e59eaf75210
Fabzus <alinbalog5@gmail.com>
2022-12-08 21:08:58 +0200

    fixed API calls

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts/", {
+      const response = await fetch("https://workoutbuddybackend-6qqe.onrender.com/api/workouts/", {
         headers: {
           'Authorization': `Bearer ${user.token}`,
         },
